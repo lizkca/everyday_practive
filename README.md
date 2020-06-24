f2064e908ac7f97a6cc64603bb7f0b79a66efa5f
lienxiong <lizkca@163.com>
2020-06-24 21:52:44 +0800

    day 2

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # everyday_practive
+learn OOP is hard, let's practive it everyday, and record it.
+day1 (2020-6-21), create a lift project.sim a lift.
+day2 (2020-6-22), 编程，就是用一个概念里的对象，它的某些个状态不断变动。
+用来模拟真实的对象。思考一下，电梯对象，它的什么状态不断变动，可以用什么
+数据结构来模拟？
--- lift.py
+++ lift.py
@@ -1,2 +1,28 @@
 # lift.py
 # writen by lizkca
+
+import time
+
+class Lift():
+
+    def __init__(self,v,current_floor):
+        self.v = v
+        self.current_floor = current_floor
+
+    def get_current_floor(self):
+        return self.current_floor
+
+    def set_current_floor(self, n):
+        self.current_floor = n
+
+    def goto_n_floor(self, n):
+        t = (n - self.current_floor) * self.v
+        time.sleep(t)
+        self.set_current_floor(n)
+
+
+
+if __name__ == '__main__':
+    my_lift = Lift(2,1)
+    my_lift.goto_n_floor(10)
+    print(my_lift.get_current_floor())
